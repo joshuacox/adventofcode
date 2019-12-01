da2461a9a456f021e6f3fd6668d1da77e11ad107
Josh Cox <josh@webhosting.coop>
2019-12-01 14:15:50 -0600

    speed

--- speed.sh
+++ speed.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 source lib/util.sh
-slurpy="$(grep -v '^#' ./test|sort -n)"
+#slurpy="$(grep -v '^#' ./test|sort -n)"
+slurpy="$(cat ./test)"
 
 main () {
-  TOTAL_WATE=0
   TOTAL_CRUNCHED=0
   while read WATE
   do
     CRUNCHED=$(cruncher $WATE)
-    let TOTAL_WATE+=$WATE
     let TOTAL_CRUNCHED+=$CRUNCHED
   done <<< "$slurpy"
   printf "TOTAL_CRUNCHED=%d\n" $TOTAL_CRUNCHED

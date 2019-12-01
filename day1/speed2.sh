3c61b9938ea76d0ac4098d8a51ed2b8071666761
Josh Cox <josh@webhosting.coop>
2019-12-01 15:09:41 -0600

    Corrected part2

--- day1/speed2.sh
+++ day1/speed2.sh
@@ -6,13 +6,10 @@ slurpy="$(cat ./test)"
 main () {
   while read WATE
   do
-    CRUNCHED=$(cruncher $WATE)
     XFUEL=$(xfuel $WATE)
     let TOTAL_XFUEL+=$XFUEL
-    let TOTAL_CRUNCHED+=$CRUNCHED
-    let TOTAL_FUEL+=$(($XFUEL + $CRUNCHED))
   done <<< "$slurpy"
-  printf "TOTAL_FUEL=%d\n" $TOTAL_FUEL
+  printf "TOTAL_XFUEL=%d\n" $TOTAL_XFUEL
 }
 
 time main $@
